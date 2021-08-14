05d20f407837e6b96d6b6cf0e06245d4504bb843
imfeel <68771752+imfeel@users.noreply.github.com>
2021-08-14 21:52:58 +0300

    Add files via upload

---
+++ BrawlAPI.class.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Brawl Api
+ *
+ * @author github.com/imfeel
+ *
+ */
+
+class BrawlApi {
+	private $token;
+	public function __construct($token = null) {
+		$this->token = $token;
+	}
+	public function EventsRotation() {
+	    return $this->requests('events/rotation');
+	}
+	public function clubs($tag) { 
+	    $tag = str_replace('#', '', $tag);
+	    return $this->requests('clubs/%' . $tag, $this->token);
+	}
+	public function clubsMembers($tag) {
+	    $tag = str_replace('#', '', $tag);
+	    return $this->requests('clubs/%' . $tag . '/members', $this->token);
+	}
+	public function brawlers($token) { 
+	    $tag = str_replace('#', '', $tag);
+	    return $this->requests('brawlers', $token);
+
+	}
+	public function brawlersId($Id) { 
+	    $tag = str_replace('#', '', $tag);
+	    return $this->requests('brawlers/' . $Id, $this->token);
+	}
+    public function players($tag) { 
+        $tag = str_replace('#', '', $tag);
+        return $this->requests('players/%' . $tag, $this->token);
+    }
+    public function BattleLog($tag) {
+        $tag = str_replace('#', '', $tag);
+        return $this->requests('players/%' . $tag . 'battlelog', $this->token);
+    }
+
+	
+	public function getAuthToken() {
+		return $this->token;
+	}	
+	
+	private function requests($url) {
+        $ch = curl_init();
+        # getAuthToken()
+        curl_setopt($ch, CURLOPT_URL, 'https://api.brawlstars.com/v1/'.$url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        
+        
+        $headers = array();
+        $headers[] = 'Accept: application/json';
+        $headers[] = 'Authorization: Bearer ' .  $this->token;
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            return 'Requests error:' . curl_error($ch);
+        }
+        
+        return $result;
+
+
+	}
+}
